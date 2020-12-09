c47b43f5908d3adf6e2d852540dbe99711cfb9ed
CeAlCuadrado <cealcuadrado@outlook.com>
2020-12-08 23:04:14 -0300

    Diseño inicial del layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './layouts/main-layout/header/header.component';
+import { FooterComponent } from './layouts/main-layout/footer/footer.component';
+import { DummyContentComponent } from './layouts/main-layout/dummy-content/dummy-content.component';
+import { MainComponent } from './layouts/main-layout/main/main.component';
+import { FooterInfoComponent } from './shared/footer-info/footer-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    DummyContentComponent,
+    MainComponent,
+    FooterInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

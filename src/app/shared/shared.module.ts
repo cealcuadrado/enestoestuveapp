5a350f11ab3d778869514a8849e539b9820a2d1d
CeAlCuadrado <cealcuadrado@outlook.com>
2020-12-16 17:23:44 -0300

    Componentes Post, Default Post, Paragraph Lorem Ipsum, contenido del post

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,20 +6,30 @@ import { PostTagsComponent } from './post/post-tags/post-tags.component';
 import { PostNotesComponent } from './post/post-notes/post-notes.component';
 import { PostCommentsComponent } from './post/post-comments/post-comments.component';
 
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { ParagraphLoremIpsumComponent } from './paragraph-lorem-ipsum/paragraph-lorem-ipsum.component';
+import { PostsComponent } from './posts/posts.component';
+import { DefaultPostComponent } from './default-post/default-post.component';
+
 @NgModule({
   declarations: [
     LoremIpsumComponent,
     PostComponent,
     PostTagsComponent,
     PostNotesComponent,
-    PostCommentsComponent
+    PostCommentsComponent,
+    ParagraphLoremIpsumComponent,
+    PostsComponent,
+    DefaultPostComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    YouTubePlayerModule
   ],
   exports: [
     LoremIpsumComponent,
-    PostComponent
+    PostComponent,
+    DefaultPostComponent
   ]
 })
 export class SharedModule { }

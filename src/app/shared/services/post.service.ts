52a4b795debcdaf790529a24d2a795f987ded066
CeAlCuadrado <cealcuadrado@outlook.com>
2020-12-16 19:59:48 -0300

    Agregados métodos de búsqueda de posts por tipo y tag

--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -2,6 +2,7 @@ import { Post } from './../interfaces/post';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,13 @@ export class PostService {
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(`${this.url}`)
+  }
+
+  getPostsByType(type: string): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.url}`).pipe(map(posts => posts.filter(post => post.type === type)));
+  }
+
+  getPostsByTag(tag: string): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.url}`).pipe(map(posts => posts.filter(post => post.tags.includes(tag))));
   }
 }

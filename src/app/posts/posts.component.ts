8c856c6c376bc175cd57819c9ccc2b4a014c6784
CeAlCuadrado <cealcuadrado@outlook.com>
2020-12-17 19:58:59 -0300

    Implementada muestra de post por ID

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,3 +1,5 @@
+import { Post } from './../shared/interfaces/post';
+import { PostService } from './../shared/services/post.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,14 +13,21 @@ export class PostsComponent implements OnInit {
   id: string;
   showMore: true;
 
+  loadedPost: Post;
+
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private post: PostService
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       if (params.id) {
         this.id = params.id;
+        let id = parseInt(this.id);
+        this.post.getPost(id).subscribe(post => {
+          this.loadedPost = post;
+        });
       }
     });
   }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,3 +1,5 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,7 +13,9 @@ import { PostsComponent } from './posts.component';
   imports: [
     CommonModule,
     PostsRoutingModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    NgbModule
   ]
 })
 export class PostsModule { }
